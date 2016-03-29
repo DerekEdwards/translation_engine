93ba43933b8e56ced06c5306c98d11fedb3fe37d
Derek Edwards <dedwards8@gmail.com>
2016-03-29 15:24:43 -0400

    Fix Primary Key for translation.rb

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -2,6 +2,8 @@ class Translation < ActiveRecord::Base
 
   belongs_to :locale
   belongs_to :translation_key
+  
+  self.primary_key = :id 
 
   delegate :name, to: :translation_key, prefix: :key, allow_nil: true
 
