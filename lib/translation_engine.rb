20d08e50f75561bf00eabd433108192d884484bb
Derek Edwards <dedwards8@gmail.com>
2017-02-16 12:59:39 -0500

    Add working load_locales to engine.

--- lib/translation_engine.rb
+++ lib/translation_engine.rb
@@ -5,7 +5,7 @@ require 'translation_engine/action_view_extensions'
 #refactor these includes at some point, if possible
 #require 'tasks/database_tasks'
 require 'tasks/install'
-require 'tasks/load'
+require 'tasks/loader'
 
 module TranslationEngine
 
