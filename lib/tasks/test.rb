6e744d3a394ff133020180b362b9ee9e5ccd21d4
Derek Edwards <dedwards8@gmail.com>
2017-02-16 12:53:49 -0500

    Add working load_locales to engine.

---
+++ lib/tasks/test.rb
@@ -0,0 +1,13 @@
+require 'rake'
+require 'active_record'
+
+namespace :translation_engine do
+
+  desc "Create translation, translation_key, and locale tables"
+  task :print_derek => :environment do
+
+    puts 'DEREK'
+
+  end
+
+end
