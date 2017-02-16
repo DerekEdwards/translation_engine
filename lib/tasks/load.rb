bf847d54308f86db8687ea202c7872590cdbd818
Derek Edwards <dedwards8@gmail.com>
2017-02-16 13:05:38 -0500

    Update load_locales

--- lib/tasks/load.rb
+++ lib/tasks/load.rb
@@ -11,34 +11,21 @@ namespace :translation_engine do
     end
   end
 
-  desc "Create translation, translation_key, and locale tables"
-  task :print_derek => :environment do
-
-    puts 'DEREK'
-
-  end
-
   desc "Load database translations from config/locales/*.yml files"
   task :load_locales => :environment do
     locales_directory = Rails.root.to_s + "/config/locales/"
 
     Dir.foreach(locales_directory) do |filename|
-
       unless filename == "." || filename == ".."
-
         puts "Loading locale file #{filename}"
-
         y = YAML.load_file(locales_directory + filename)
-
         failed = success = skipped = 0
-        
-        y.each_with_parents do |parents, v|
 
+        y.each_with_parents do |parents, v|
           locale = parents.shift
           locale = Locale.find_or_create_by(name: locale)
 
           if v.is_a? Array
-
             translation_key_name = parents.join('.')
             translation_value = v.join(',')
             translation_key = TranslationKey.find_or_create_by!(name: translation_key_name)
@@ -47,25 +34,17 @@ namespace :translation_engine do
             existing_translation = Translation.where("translation_key_id = ? AND locale_id = ?", translation_key.id, locale.id)
 
             if existing_translation.count == 0
-
               new_translation = Translation.new
-
               new_translation.translation_key_id = translation_key.id
               new_translation.locale_id = locale.id
-
               new_translation.value = translation_value
               new_translation.save!
-
               new_translation.id.nil? ? failed += 1 : success += 1
-
             else
-
               skipped += 1
-
             end
 
           else
-
             translation_key_name = parents.join('.')
             translation_value = v
             translation_key = TranslationKey.find_or_create_by!(name: translation_key_name)
@@ -74,32 +53,20 @@ namespace :translation_engine do
             existing_translation = Translation.where("translation_key_id = ? AND locale_id = ?", translation_key.id, locale.id)
 
             if existing_translation.count == 0
-
               new_translation = Translation.new
-
               new_translation.translation_key_id = translation_key.id
               new_translation.locale_id = locale.id
-
               new_translation.value = translation_value
               new_translation.save!
-
               new_translation.id.nil? ? failed += 1 : success += 1
-
             else
-
               skipped += 1
-
             end
-
-          end
-
-        end
-
+          end # v.is? Array 
+        
+        end # y.each 
         puts "Read #{success+failed} keys, #{success} successful, #{failed} failed, #{skipped} skipped"
-
-      end
-
-    end
-  end
-
-end
+      end # unless filename
+    end # Dir.foreach
+  end #load_locaales
+end #translation_engine
